b5e394ed008f4eb32f836945614289ce4d586cab
Shohei Hagiwara <hagiwara.shohei@gmail.com>
2019-05-24 13:32:24 +0900

    Changed match value calculation algorithm.

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+# -*- encoding: utf-8 -*-
 """aaa.txtを簡単に目的のものを検索する。
 
 複数の単語を入力し、50行中でそれらの単語がいくつ出てくるかをチェックし、
@@ -6,10 +8,11 @@
 
 import tkinter as tk
 import os
+from functools import reduce
 
 PATH = 'test_data/aaa.txt'
 ENCODING = 'utf-8'
-SEARCH_STEP = 30
+SEARCH_STEP = 50
 TOP_N = 3
 SEARCH_KEY_BINDING = '<Control-Key-f>'
 
@@ -34,6 +37,7 @@ class AaaTxtSearcher(tk.Tk):
         """検索対象の単語の出現回数を返す
 
         大文字小文字は関係なく検索する。
+        ただし、大文字で検索したときには、区別して検索する。
 
         :param words: 検索対象の単語
         :param lines: ここから検索対象の単語を探す
@@ -43,34 +47,72 @@ class AaaTxtSearcher(tk.Tk):
         count = 0
         for word in words:
             for line in lines:
-                count += line.upper().count(word.upper())
+                if word.isupper():
+                    count += line.count(word)
+                else:
+                    count += line.lower().count(word.lower())
 
         return count
 
+    @staticmethod
+    def __calc_math_val(words, lines):
+        """calclate match value.
+
+        match value is defined as follows.
+
+        マッチ度 = Π( M(i) )
+        M(i) = 検索ワードのi番目についてのテキストのマッチ度を表す。 = 1 + h
+            h: ヒット係数。マックス5回で、ヒットするたびに高くなる。5以上は変わらない。
+                ヒット回数    0  1  2  3  4  5  6  7 ...  n
+                h       -0.2 .1 .2 .3 .4 .5 .5 .5 ... .5
+
+        :param words:
+        :param lines:
+        :return:
+        """
+
+
+        match_val_for_each_word = []
+
+        for each_word in words:
+            word_count = AaaTxtSearcher.__count_words_in_lines([each_word], lines)
+            if word_count == 0:
+                coefficient = -0.2
+            elif word_count > 5:
+                coefficient = 0.5
+            else:
+                coefficient = word_count / 10
+            match_val_for_each_word.append(1 + coefficient)
+
+        return reduce(lambda x, y: x*y, match_val_for_each_word)
+
+
     def search(self, event=None):
+        """検索
+        """
 
         # 単語を読み込んでリストにする
         search_word_list = self.query.get('1.0', tk.END).split()
 
-        if len(search_word_list) == 0:
+        if len(search_word_list) == 0: 
             return
 
         with open(PATH, encoding=ENCODING) as f:
 
-            lines = f.readlines()
+            aaa_txt_lines = f.readlines()
 
-            # 検索対象単語が一番多く出現する範囲を見つける
+            # マッチ度をだす。
             running_best_score = None
             running_best_score_start_line_num = None
             ranking = []
-            for start_line_num in range(0, len(lines), SEARCH_STEP):
-                end_line_num = start_line_num + SEARCH_STEP if start_line_num + SEARCH_STEP < len(lines) else len(lines) -1
-                tmp_count = self.__count_words_in_lines(search_word_list, lines[start_line_num:end_line_num])
+            for start_line_num in range(0, len(aaa_txt_lines), SEARCH_STEP):
+                end_line_num = start_line_num + SEARCH_STEP if start_line_num + SEARCH_STEP < len(aaa_txt_lines) else len(aaa_txt_lines) -1
+                match_val = self.__calc_math_val(search_word_list, aaa_txt_lines[start_line_num:end_line_num])
 
-                ranking.append((tmp_count, "".join(lines[start_line_num:end_line_num])))
+                ranking.append((match_val, "".join(aaa_txt_lines[start_line_num:end_line_num]), start_line_num, end_line_num))
 
-                if tmp_count > 0 and (running_best_score is None or tmp_count > running_best_score):
-                    running_best_score = tmp_count
+                if match_val > 0 and (running_best_score is None or match_val > running_best_score):
+                    running_best_score = match_val
                     running_best_score_start_line_num = start_line_num
 
             self.answer.delete('1.0', tk.END)
@@ -81,9 +123,14 @@ class AaaTxtSearcher(tk.Tk):
             else:
                 start = running_best_score_start_line_num
                 end = running_best_score_start_line_num + SEARCH_STEP - 1
-                result = ''.join(lines[start:end])
+                result = ''.join(aaa_txt_lines[start:end])
 
             ranking.sort(key=lambda tup: tup[0], reverse=True)
+
+            for each_ranking_entry in ranking:
+                match_val = each_ranking_entry[0]
+                start_line_num = each_ranking_entry[2]
+                print("match val: [{}] start line number: [{}]".format(match_val, start_line_num))
 
             result_list = [tuple[1] for tuple in ranking[0:TOP_N]]
             line_delimiter = os.linesep + '*' * 80 + os.linesep * 2
